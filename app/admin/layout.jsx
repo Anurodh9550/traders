870cc74ce8ed70477fa20cb74b3835be5d587b2b
Anurodh Kumar <anurodhkumar123@gmail.com>
2025-11-13 14:52:13 +0530

    Project updated

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,12 +1,9 @@
-export const metadata = {
-  title: "Admin Dashboard | Pioneer Wealth",
-  description: "Admin panel without Navbar and Footer",
-};
-
 export default function AdminLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gray-50 min-h-screen">{children}</body>
+      <body className="bg-gray-50 flex flex-col min-h-screen">
+        {children}
+      </body>
     </html>
   );
 }
